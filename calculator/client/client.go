b54504c9f633f55ee8c0528d53bf9eeb253078f0
Cosmin Potocean <cosmycx@gmail.com>
2019-01-28 16:13:01 -0500

    added streaming client

--- calculator/client/client.go
+++ calculator/client/client.go
@@ -20,13 +20,43 @@ func main() {
 
 	c := calc_pb.NewCalculatorServiceClient(conn)
 
-	rpcCalcSum(c, int32(10), int32(15))
+	doSumServ(c, int32(10), int32(15))
 
-	rpcStreamingFiboNums(c, int32(15))
+	doFibonacciServ(c, int32(15))
+
+	doMeanServ(c)
 
 } // .main
 
-func rpcStreamingFiboNums(cc calc_pb.CalculatorServiceClient, endNum int32) {
+func doMeanServ(c calc_pb.CalculatorServiceClient) {
+	fmt.Println("Starting client streaming of numbers...")
+
+	//MeanService(ctx context.Context, opts ...grpc.CallOption) (CalculatorService_MeanServiceClient, error)
+
+	numbers := []int64{1, 3, 9, 91, 504, 1023}
+
+	stream, err := c.MeanService(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling MeanService: %v", err)
+	}
+
+	for _, number := range numbers {
+		fmt.Printf("Sending number: %v\n", number)
+		stream.Send(&calc_pb.MeanRequest{
+			Number: number,
+		})
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error while receiving response: %v", err)
+	}
+
+	fmt.Printf("The Mean is: %v\n", res.GetMean())
+
+} // .doMeanServ
+
+func doFibonacciServ(cc calc_pb.CalculatorServiceClient, endNum int32) {
 	fmt.Println("Starting fibonaci request...")
 	req := &calc_pb.FibonacciRequest{
 		Num: endNum,
@@ -48,9 +78,9 @@ func rpcStreamingFiboNums(cc calc_pb.CalculatorServiceClient, endNum int32) {
 		log.Printf("Response from Fibonacci: %v", msg.GetResultNum())
 	} // .for
 
-} // .rpcStreamingFiboNums
+} // .doFibonacciServ
 
-func rpcCalcSum(cc calc_pb.CalculatorServiceClient, a int32, b int32) int32 {
+func doSumServ(cc calc_pb.CalculatorServiceClient, a int32, b int32) int32 {
 	fmt.Println("Starting sum request ...")
 
 	req := &calc_pb.SumRequest{
@@ -66,4 +96,4 @@ func rpcCalcSum(cc calc_pb.CalculatorServiceClient, a int32, b int32) int32 {
 
 	log.Printf("Sum: %v", res.Result)
 	return res.Result
-} // .rpcCalcSum
+} // .doSumServ
